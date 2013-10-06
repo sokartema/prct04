d26c7d99f537258dafc592fdbe5f48765b9828ff
Jazer Abreu <artema@artema-VirtualBox.(none)>
2013-10-06 23:13:55 +0100

    Segunda version inicializacion matriz c

--- prct04.rb
+++ prct04.rb
@@ -3,3 +3,13 @@
 a = [[2, 4, 5], [3, 4, 5], [2, 1, 5]]   
 b = [[7, 3, 5], [2, 1, 2], [3, 5, 7]]   
 
+c = Array.new                          
+
+m, n = a.size, a[1].size               
+for i in 0...m do                       
+   c[i] = Array.new                    
+   for j in 0...n do                  
+      c[i][j] = 0                     
+   end  
+end  
+
