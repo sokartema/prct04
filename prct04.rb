09f5cad7c2c2faa21b0012d3577902b57d67e60b
Jazer Abreu <artema@artema-VirtualBox.(none)>
2013-10-08 08:54:30 +0100

    Version final sin ningun tipo de clase

--- prct04.rb
+++ prct04.rb
@@ -2,16 +2,10 @@
 
 a = [[3, 6, 2], [4, 0, 3], [0, 1, 1]] 
 b = [[5, 2, 2], [6, 4, 7], [2, 2, 0]]   
-c = Array.new                           
+c = [[0,0,0],[0,0,0],[0,0,0]]                          
 
 m, n = a.size, a[1].size                
-for i in 0...m do                      
-   c[i] = Array.new                    
-  
-   for j in 0...n do                    
-      c[i][j] = 0   
-   end  
-end  
+
 
 
 for z in 0...m do
